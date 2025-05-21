316a186e2f7297828e8ceee937dad27c59bec3fc
iagocoding2317 <115094044+iagocoding2317@users.noreply.github.com>
2025-05-21 11:45:06 -0300

    Add files via upload

---
+++ Porta.java
@@ -0,0 +1,46 @@
+public class Porta {
+    private String cor;
+    private int altura;
+    private int largura; 
+    private boolean estaAberta;
+    
+    public Funcionario(String cor, int altura, int largura, boolean estaAberta) {
+        this.cor = cor;
+        this.altura = altura;
+        this.largura = largura;
+        this.estaAberta = estaAberta;
+    }
+
+   public String getCor(){
+     return cor;
+   }
+
+   public int getAltura{
+     return altura;
+   }
+
+   public int getLargura{
+     return largura;
+   }
+
+   public boolean getEstaAberta(){
+    return estaAberta;
+   }
+
+   public void setCor(String cor){
+    this.cor = cor;
+   }
+
+   public void setAltura(int altura){
+    this.altura = altura;
+   }
+   
+   public void setLargura(int largura){
+    this.largura = largura
+   }
+
+   public String toString(){
+
+    return String.format("A cor é %s, a altura é %d, a largura é %d, a porta está aberta?:   nome, aniversario);
+  }
+}
